18584aad7935817f209506b0043be90e0c38857f
0xfederico <79773985+0xfederico@users.noreply.github.com>
2021-03-01 23:48:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,27 @@ def get_usernames(message):
     usr_list = list()
     for usr in re.finditer(rfilter, message.text):
         usr_list.append(usr.group())
-
-    if not usr_list:
-        bot.send_message(message, f'No username found for {message.text}')
-        raise IndexError
     return usr_list
 
 
-def get_pro_pic_hd(p_url):
+def get_pro_pic_hd(message, p_url):
     response = requests.get(f'{p_url}/?__a=1', headers={"User-Agent": "Mozilla/5.0"})
-    json_data = json.loads(response.text)
-    return json_data['graphql']['user']['profile_pic_url_hd']
-
+    try:
+        json_data = json.loads(response.text)
+    except json.decoder.JSONDecodeError:
+        bot.reply_to(message, "Response string could not be converted to JSON, please try again later")
+        return None
+    else:
+        return json_data['graphql']['user']['profile_pic_url_hd']
 
 @bot.message_handler(regexp=rfilter)
 def send_pic(message):
     usernames = get_usernames(message)
     for u in usernames:
         profile_url = f'https://www.instagram.com/{u[1:]}'
-        bot.send_photo(message.chat.id, get_pro_pic_hd(profile_url))
+        pic_url = get_pro_pic_hd(message, profile_url)
+        if pic_url is not None:
+            bot.send_photo(message.chat.id, pic_url)
 
 
 bot.polling()
