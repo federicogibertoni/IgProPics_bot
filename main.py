ce5f5823145fb5a47c4835eab8e88e0b5350dab2
0xfederico <79773985+0xfederico@users.noreply.github.com>
2021-03-01 22:07:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import telebot
 import os, time, re, requests, json
 
 bot = telebot.TeleBot(token=os.environ['IGPROPIC_TOKEN'])
-rfilter = re.compile(r'(@\w+)+')
+rfilter = r'(@\w+)+'
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Ciao")
+    bot.reply_to(message, "Hi")
 
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Send multiple Ig username to get some information! Remember to use '@'!")
+    bot.reply_to(message, "Send me multiple Ig usernames to get some information! Remember to use '@'!")
 
 
 def get_usernames(message):
     usr_list = list()
-    for usr in rfilter.finditer(message.text):
+    for usr in re.finditer(rfilter, message.text):
         usr_list.append(usr.group())
 
     if not usr_list:
@@ -31,16 +32,13 @@ def get_pro_pic_hd(p_url):
     return json_data['graphql']['user']['profile_pic_url_hd']
 
 
-@bot.message_handler(func=lambda msg: msg.text is not None and rfilter.match(msg.text))
+@bot.message_handler(regexp=rfilter)
 def send_pic(message):
+    print(message)
     usernames = get_usernames(message)
     for u in usernames:
         profile_url = f'https://www.instagram.com/{u[1:]}'
         bot.send_photo(message.chat.id, get_pro_pic_hd(profile_url))
 
 
-while True:
-    try:
-        bot.polling()
-    except Exception:
-        time.sleep(5)
+bot.polling()
