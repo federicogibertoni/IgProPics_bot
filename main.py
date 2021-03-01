81b2139ccd97896aa451500a94b204617cf4b15a
0xfederico <79773985+0xfederico@users.noreply.github.com>
2021-03-01 19:33:26 +0100

    Multiple username support

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import telebot
-import os
-import time
-import re
-import requests
-import json
+import os, time, re, requests, json
 
 bot = telebot.TeleBot(token=os.environ['IGPROPIC_TOKEN'])
-
+rfilter = re.compile(r'(@\w+)+')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -15,13 +11,12 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Send an Ig username to get some information! Remember to use '@'!")
+    bot.reply_to(message, "Send multiple Ig username to get some information! Remember to use '@'!")
 
 
-def get_username(message):
+def get_usernames(message):
     usr_list = list()
-    p = re.compile(r'@\w+')
-    for usr in p.finditer(message.text):
+    for usr in rfilter.finditer(message.text):
         usr_list.append(usr.group())
 
     if not usr_list:
@@ -30,18 +25,18 @@ def get_username(message):
     return usr_list
 
 
-def get_pro_pic_hd(message, pro_name):
+def get_pro_pic_hd(pro_name):
     response = requests.get(f'{pro_name}/?__a=1', headers={"User-Agent": "Mozilla/5.0"})
     json_data = json.loads(response.text)
     return json_data['graphql']['user']['profile_pic_url_hd']
 
 
-@bot.message_handler(func=lambda msg: msg.text is not None and re.match(r'@\w+', msg.text))
+@bot.message_handler(func=lambda msg: msg.text is not None and rfilter.match(msg.text))
 def send_pic(message):
-    l = get_username(message)
-    profile_name = 'https://www.instagram.com/{}'.format(l[0][1:])
-
-    bot.send_photo(message.chat.id, get_pro_pic_hd(message, profile_name))
+    usernames = get_usernames(message)
+    for u in usernames:
+        profile_url = f'https://www.instagram.com/{u[1:]}'
+        bot.send_photo(message.chat.id, get_pro_pic_hd(profile_name))
 
 
 while True:
